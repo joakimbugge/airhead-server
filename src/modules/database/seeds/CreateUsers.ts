db625b7ae443d78214a745637f5b52a6e17adee4
Joakim Bugge <joakim.bugge@gmail.com>
2019-11-16 23:34:14 +0100

    Improve product seeds

--- src/modules/database/seeds/CreateUsers.ts
+++ src/modules/database/seeds/CreateUsers.ts
@@ -7,7 +7,7 @@ import { User } from '../../user/domain/User';
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
     await factory(User)().map(async (user: User) => {
-      user.products = await factory(Product)().seedMany(2);
+      user.products = await factory(Product)().seedMany(10);
       return user;
     }).seedMany(5);
   }
