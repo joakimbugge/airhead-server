a78d26f3fa26d251e50bc1b8ad81780968d3056b
Joakim Bugge <joakim.bugge@gmail.com>
2020-02-08 20:08:07 +0100

    Add initial pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: 'dist/main.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '157.230.122.90',
+      ref: 'origin/pm2-deploy',
+      repo: 'git@github.com:joakimbugge/airhead-server.git',
+      path: '/root/apps',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
