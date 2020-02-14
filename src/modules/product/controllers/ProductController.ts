03d49c217c71f3d6a174b906eab2156436ff1546
Joakim Bugge <joakim.bugge@gmail.com>
2020-02-15 00:38:53 +0100

    Add products documentation

--- src/modules/product/controllers/ProductController.ts
+++ src/modules/product/controllers/ProductController.ts
@@ -10,7 +10,12 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ValidationError } from 'class-validator';
+import { BAD_REQUEST, CREATED, NOT_FOUND, OK, UNAUTHORIZED } from 'http-status-codes';
+import { ApiBadRequestException } from '../../../../doc/exceptions/ApiBadRequestException';
+import { ApiNotFoundException } from '../../../../doc/exceptions/ApiNotFoundException';
+import { ApiUnauthorizedException } from '../../../../doc/exceptions/ApiUnauthorizedException';
 import { Authed } from '../../../server/decorators/Authed';
 import { Authenticated } from '../../../server/decorators/Authenticated';
 import { User } from '../../user/domain/User';
@@ -21,26 +26,32 @@ import { UpdateProductDto } from '../dtos/UpdateProductDto';
 import { ProductService } from '../services/ProductService';
 
 @Controller('products')
+@ApiTags('products')
 export class ProductController {
-  private MINE_TERM_LENGTH = 3;
-  private MIN_LIKENESS = 10;
+  private readonly MINE_TERM_LENGTH = 3;
+  private readonly MIN_LIKENESS = 10;
 
   constructor(private readonly productService: ProductService) {
   }
 
   @Get('/')
   @Authenticated()
+  @ApiResponse({ status: OK, type: [Product] })
+  @ApiResponse({ status: UNAUTHORIZED, type: ApiUnauthorizedException })
   public findMany(@Authed() user: User): Promise<Product[]> {
     return this.productService.findMany({ user });
   }
 
   @Get('/search')
   @Authenticated()
+  @ApiQuery({ name: 'minLikeness', required: false, type: Number })
+  @ApiResponse({ status: OK, type: [FuzzySearchItem] })
+  @ApiResponse({ status: UNAUTHORIZED, type: ApiUnauthorizedException })
   public search(
     @Query('term') term: string,
     @Query('minLikeness', new ParseIntPipe()) minLikeness = this.MIN_LIKENESS,
-    @Authed() user: User): Promise<Array<FuzzySearchItem<Product>>> {
-
+    @Authed() user: User,
+  ): Promise<FuzzySearchItem<Product>[]> {
     if (!term || term.length < this.MINE_TERM_LENGTH) {
       throw this.createSearchValidationError();
     }
@@ -50,12 +61,18 @@ export class ProductController {
 
   @Get('/:id')
   @Authenticated()
+  @ApiResponse({ status: OK, type: Product })
+  @ApiResponse({ status: NOT_FOUND, type: ApiNotFoundException })
+  @ApiResponse({ status: UNAUTHORIZED, type: ApiUnauthorizedException })
   public get(@Param('id', new ParseIntPipe()) id: number, @Authed() user: User): Promise<Product> {
     return this.productService.get({ id, user });
   }
 
   @Post('/')
   @Authenticated()
+  @ApiResponse({ status: CREATED, type: Product })
+  @ApiResponse({ status: BAD_REQUEST, type: ApiBadRequestException })
+  @ApiResponse({ status: UNAUTHORIZED, type: ApiUnauthorizedException })
   public create(@Body() createProductDto: CreateProductDto, @Authed() user: User): Promise<Product> {
     const product = new Product();
 
@@ -69,9 +86,14 @@ export class ProductController {
 
   @Put('/:id')
   @Authenticated()
-  public async update(@Param('id', new ParseIntPipe()) id: number,
-                      @Body() updateProductDto: UpdateProductDto,
-                      @Authed() user: User): Promise<Product> {
+  @ApiResponse({ status: OK, type: Product })
+  @ApiResponse({ status: BAD_REQUEST, type: ApiBadRequestException })
+  @ApiResponse({ status: UNAUTHORIZED, type: ApiUnauthorizedException })
+  public async update(
+    @Param('id', new ParseIntPipe()) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+    @Authed() user: User,
+  ): Promise<Product> {
     const product = await this.productService.get({ id, user });
 
     product.name = updateProductDto.name;
@@ -83,6 +105,9 @@ export class ProductController {
 
   @Delete('/:id')
   @Authenticated()
+  @ApiResponse({ status: OK, type: Product })
+  @ApiResponse({ status: NOT_FOUND, type: ApiNotFoundException })
+  @ApiResponse({ status: UNAUTHORIZED, type: ApiUnauthorizedException })
   public async delete(@Param('id', new ParseIntPipe()) id: number, @Authed() user: User): Promise<Product> {
     const product = await this.productService.get({ id }, { relations: ['user'] });
 
