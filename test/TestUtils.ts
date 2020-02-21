d23a61b76d2ccc8c15a48ae402fe96c401d857c6
Joakim Bugge <joakim.bugge@gmail.com>
2020-02-21 01:15:58 +0100

    Clean up tests

--- test/TestUtils.ts
+++ test/TestUtils.ts
@@ -1,40 +1,35 @@
 import { INestApplication } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
-import { getRepository } from 'typeorm';
 import { LogService } from '../src/modules/logging/services/LogService';
-import { Product } from '../src/modules/product/domain/Product';
-import { ProductImage } from '../src/modules/product/domain/ProductImage';
 import { StorageService } from '../src/modules/shared/services/StorageService';
-import { User } from '../src/modules/user/domain/User';
 import { getExceptionFilters, getInterceptors, getPipes } from '../src/server/helpers';
 import { metadata } from '../src/server/metadata';
-import { HashUtils } from '../src/utils/HashUtils';
 
 export abstract class TestUtils {
-  public static async createModule(): Promise<TestingModule> {
-    const storageService = {
-      composeCdnUrl: () => '',
-      upload: () => new Promise(resolve => resolve()),
-      delete: () => new Promise(resolve => resolve()),
-    };
+  public static storageService = {
+    composeCdnUrl: () => '',
+    upload: () => new Promise(resolve => resolve()),
+    delete: () => new Promise(resolve => resolve()),
+  };
 
-    const logService = {
-      info: () => null,
-      error: () => null,
-    };
+  public static logService = {
+    info: () => null,
+    error: () => null,
+  };
 
-    return await Test.createTestingModule(metadata)
-                     .overrideProvider(StorageService)
-                     .useValue(storageService)
-                     .overrideProvider(LogService)
-                     .useValue(logService)
-                     .compile();
+  public static createApplication(): Promise<TestingModule> {
+
+    return Test.createTestingModule(metadata)
+               .overrideProvider(StorageService)
+               .useValue(this.storageService)
+               .overrideProvider(LogService)
+               .useValue(this.logService)
+               .compile();
   }
 
   public static async startApplication(): Promise<INestApplication> {
-    const app = (await TestUtils.createModule()).createNestApplication();
+    const app = (await this.createApplication()).createNestApplication();
     const logService = app.get(LogService);
     const { httpAdapter } = app.get(HttpAdapterHost);
 
@@ -45,61 +40,7 @@ export abstract class TestUtils {
     return await app.init();
   }
 
-  public static async shutdown(app: INestApplication | TestingModule): Promise<void> {
-    await app.close();
-  }
-
-  public static createUser(username: string, password: string = 'sample'): User {
-    const user = new User();
-
-    user.username = username;
-    user.password = HashUtils.createHash(password);
-    user.email = `${username}@example.org`;
-
-    return user;
-  }
-
-  public static persistUser(username: string = 'john'): Promise<User> {
-    return getRepository(User).save(TestUtils.createUser(username, '123'));
-  }
-
-  public static loginUser(user: User, app: INestApplication): Promise<string> {
-    return new Promise(async resolve => {
-      request(app.getHttpServer())
-        .post('/login')
-        .send({ username: user.username, password: '123' })
-        .then(res => {
-          resolve(res.body.token);
-        });
-    });
-  }
-
-  public static createProduct(name: string, user: User): Product {
-    const product = new Product();
-
-    product.name = name;
-    product.amount = 1;
-    product.amountThreshold = 2;
-    product.user = user;
-    product.images = [];
-
-    return product;
-  }
-
-  public static persistProduct(product: Product): Promise<Product> {
-    return getRepository('product').save(product);
-  }
-
-  public static createProductImage(product: Product): ProductImage {
-    const image = new ProductImage();
-    image.name = 'test.jpg';
-    image.path = '';
-    image.product = product;
-
-    return image;
-  }
-
-  public static persistProductImage(productImage: ProductImage): Promise<ProductImage> {
-    return getRepository(ProductImage).save(productImage);
+  public static stopApplication(app: INestApplication | TestingModule): Promise<void> {
+    return app.close();
   }
 }
--- test/units/services/ProductServiceTest.ts
+++ test/units/services/ProductServiceTest.ts
@@ -1,144 +1,31 @@
 import { TestingModule } from '@nestjs/testing';
-import * as faker from 'faker';
-import { getRepository } from 'typeorm';
-import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
-import { Product } from '../../../src/modules/product/domain/Product';
 import { ProductService } from '../../../src/modules/product/services/ProductService';
 import { User } from '../../../src/modules/user/domain/User';
+import { TestHelpers } from '../../TestHelpers';
 import { TestUtils } from '../../TestUtils';
 
 let app: TestingModule;
 let service: ProductService;
 let user: User;
 
-const persistProduct = (name: string = faker.commerce.productName()): Promise<Product> =>
-  getRepository(Product).save(TestUtils.createProduct(name, user));
-
-const persistProducts = async (products: Product[]): Promise<void> => {
-  for (const product of products) {
-    await getRepository(Product).save(product);
+const persistProducts = async (names: string[]): Promise<void> => {
+  // tslint:disable-next-line:prefer-for-of
+  for (let i = 0; i < names.length; i++) {
+    await TestHelpers.persistProduct(TestHelpers.createProduct(user, names[i]));
   }
 };
 
 beforeEach(async () => {
-  app = await TestUtils.createModule();
-  service = app.get<ProductService>(ProductService);
-  user = await getRepository(User).save(TestUtils.createUser('Lisa'));
+  app = await TestUtils.createApplication();
+  service = app.get(ProductService);
+  user = await TestHelpers.persistUser(TestHelpers.createUser());
 });
 
-afterEach(() => TestUtils.shutdown(app));
-
-describe('save()', () => {
-  test('Returns reference to original', async () => {
-    const product = TestUtils.createProduct('Milk', user);
-    const savedProduct = await service.save(product);
-
-    expect(product).toBe(savedProduct);
-  });
-
-  test('Saves to database', async () => {
-    const savedProduct = await service.save(TestUtils.createProduct('Milk', user));
-    const fetchedProduct = await getRepository(Product).findOne({ id: savedProduct.id });
-
-    expect(fetchedProduct).not.toBeUndefined();
-    expect(fetchedProduct.id).toEqual(savedProduct.id);
-  });
-
-  test('Updates in database', async () => {
-    const product = TestUtils.createProduct('Milk', user);
-    const savedProduct = { ...await service.save(product) };
-
-    product.name = 'berries';
-    await service.save(product);
-    const fetchedProduct = getRepository(Product).findOne({ id: savedProduct.id });
-
-    expect(fetchedProduct).not.toEqual(savedProduct);
-  });
-});
-
-describe('findMany()', () => {
-  test('Finds many products', async () => {
-    const products = [
-      TestUtils.createProduct('Butter', user),
-      TestUtils.createProduct('Bread', user),
-      TestUtils.createProduct('Apple', user),
-    ];
-
-    await persistProducts(products);
-
-    const foundProducts = await service.findMany({ user });
-
-    expect(foundProducts).toHaveLength(products.length);
-  });
-
-  test('Return empty array if no products are found', async () => {
-    const products = await service.findMany({ user });
-
-    expect(products).toHaveLength(0);
-  });
-
-  test('Exclude deleted products', async () => {
-    const products = [
-      TestUtils.createProduct('Butter', user),
-      TestUtils.createProduct('Bread', user),
-      TestUtils.createProduct('Apple', user),
-    ];
-
-    await persistProducts(products);
-
-    expect(await service.findMany({ user })).toHaveLength(3);
-
-    const product = await getRepository(Product).findOne({ name: 'Butter' });
-    const deletedProduct = await service.delete(product);
-
-    expect(await service.findMany({ user })).toHaveLength(2);
-    expect(product).toEqual(deletedProduct);
-  });
-});
-
-describe('get()', () => {
-  test('Gets by properties', async () => {
-    const product = await getRepository(Product).save(TestUtils.createProduct('Milk', user));
-    const gottenProduct = await service.get({ name: 'Milk' });
-
-    expect(gottenProduct.id).toEqual(product.id);
-  });
-
-  test('Throws exception if not gotten by properties', async () => {
-    const getProduct = () => service.get({ name: 'not-found' });
-
-    await expect(getProduct()).rejects.toThrow(EntityNotFoundError);
-  });
-
-  test('Throws exception if product is deleted', async () => {
-    const createdProduct = await TestUtils.persistProduct(TestUtils.createProduct('Apples', user));
-    await service.delete(createdProduct);
-
-    const getProduct = () => service.get({ name: 'Apples', user });
-
-    await expect(getProduct()).rejects.toThrow(EntityNotFoundError);
-  });
-});
-
-describe('delete()', () => {
-  test('Soft-delete product', async () => {
-    const createdProduct = await persistProduct('crackers');
-    const product = await service.get({ name: createdProduct.name });
-    const deletedProduct = await service.delete(product);
-
-    expect(deletedProduct.deletedAt).not.toBeNull();
-  });
-});
+afterEach(() => TestUtils.stopApplication(app));
 
 describe('search()', () => {
   it('Find relevant products', async () => {
-    const products = [
-      TestUtils.createProduct('Butter', user),
-      TestUtils.createProduct('Bread', user),
-      TestUtils.createProduct('Apple', user),
-    ];
-
-    await persistProducts(products);
+    await persistProducts(['Butter', 'Bread', 'Apple']);
 
     const searchResults = await service.search('Butt', user);
 
@@ -146,25 +33,19 @@ describe('search()', () => {
     // The first product should have a high likeness
     expect(searchResults[0].likeness).toBeGreaterThan(75);
     // Should not include products with likeness below threshold
-    expect(searchResults[searchResults.length - 1].likeness).not.toBeLessThan(10);
+    expect(TestHelpers.getLast(searchResults).likeness).not.toBeLessThan(10);
     // Should be sorted descending by likeness
     expect(searchResults[0].likeness).toBeGreaterThan(searchResults[1].likeness);
   });
 
   it('Find more products', async () => {
-    const products = [
-      TestUtils.createProduct('Butter', user),
-      TestUtils.createProduct('Bread', user),
-      TestUtils.createProduct('Apple', user),
-    ];
-
-    await persistProducts(products);
+    await persistProducts(['Butter', 'Bread', 'Apple']);
 
     const searchResults = await service.search('Butt', user, 0);
 
     // Should include more products because threshold is set to 0
-    expect(searchResults).toHaveLength(products.length);
+    expect(searchResults).toHaveLength(3);
     // Should include products with low likeness
-    expect(searchResults[searchResults.length - 1].likeness).toBeLessThan(10);
+    expect(TestHelpers.getLast(searchResults).likeness).toBeLessThan(10);
   });
 });
