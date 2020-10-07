011671c97db47e223ce5c271a70b647b688831fb
Joakim Bugge <joakim.bugge@gmail.com>
2020-10-07 19:06:36 +0200

    chore: replace tslint with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsConfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:prettier/recommended'
+  ],
+};
